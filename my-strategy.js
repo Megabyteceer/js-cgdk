6e721c508489dfe120d280cafad563f5113ec275
Megabyteceer <vasiliy.p.kostin@gmail.com>
2016-11-11 15:26:05 +0500

    readme

--- my-strategy.js
+++ my-strategy.js
@@ -1,7 +1,6 @@
 /**
  * Created by Megabyte on 09.11.2016.
  */
-
 const ActionType = require('./model/action-type.js');
 const BonusType = require('./model/bonus-type.js');
 const BuildingType = require('./model/building-type.js');
@@ -11,13 +10,7 @@ const ProjectileType = require('./model/projectile-type.js');
 const SkillType = require('./model/skill-type.js');
 const StatusType = require('./model/status-type.js');
 
-
-
-
 //static (available between strategies) variables here
-
-
-
 
 
 module.exports.getInstance =function () {
@@ -26,15 +19,16 @@ module.exports.getInstance =function () {
 
 
 
+    return function move(self, world, game, move) {
 
 
-
-    return function (self, world, game, move) {
 
         move.speed = game.wizardForwardSpeed;
         move.strafeSpeed = game.wizardStrafeSpeed;
         move.turn = game.wizardMaxTurnAngle;
         move.action = ActionType.MagicMissile;
+
+
 
     }
 };
